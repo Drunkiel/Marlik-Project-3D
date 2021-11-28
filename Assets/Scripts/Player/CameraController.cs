7537ed7a28cf6f7fc466172fd0daf478e8ba3823
Drunkiel <drunkielfel@gmail.com>
2021-11-28 16:43:20 +0100

    Added new functionality camera and movement

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -3,17 +3,45 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject Player;
+    public Camera Cam;
 
-    public float TimeSmooth;
     public float Xplus, Yplus, Zplus;
     private Vector3 currVelocity;
+
+    Vector3 previousPosition;
+
+    void Start(){
+
+        Cam.transform.position = Player.transform.position;
+        Cam.transform.Translate(new Vector3(0, 2, -20));
+    }
 
     // Update is called once per frame
     void Update()
     {
+        MoveCamera();
+        RotateCamera();
+    }
 
-        Vector3 newCameraPosition = new Vector3(Player.transform.position.x + Xplus, Player.transform.position.y + Yplus, Player.transform.position.z + Zplus);
-        
-        transform.position = Vector3.SmoothDamp(transform.position, newCameraPosition, ref currVelocity, TimeSmooth);
+    void MoveCamera(){
+
+        Cam.transform.position = Player.transform.position;
+        Cam.transform.Translate(new Vector3(0, 2, -20)); 
+    }
+    void RotateCamera(){
+
+        if(Input.GetMouseButtonDown(1)){
+            previousPosition = Cam.ScreenToViewportPoint(Input.mousePosition);
+        }
+
+        if(Input.GetMouseButton(1)){
+            
+            Vector3 Direction = previousPosition - Cam.ScreenToViewportPoint(Input.mousePosition);
+            Cam.transform.Rotate(new Vector3(0, 1, 0), -Direction.x * 180, Space.World);
+
+            MoveCamera();
+
+            previousPosition = Cam.ScreenToViewportPoint(Input.mousePosition);
+        }
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -18,6 +18,7 @@ public class PlayerMovement : MonoBehaviour
 
     void Update(){
         KeyMovement();
+        MouseMovement();
     }
     
     void KeyMovement(){
